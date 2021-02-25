5abeec15f13c5c53412566fa3d64f5739e081991
Carlos H Kruger <carloshkruger@gmail.com>
2021-02-25 16:13:30 -0300

    ensure AuthUseCase returns null if LoadUserByEmailRepository returns null

--- src/domain/usecases/auth-usecase.spec.js
+++ src/domain/usecases/auth-usecase.spec.js
@@ -22,7 +22,11 @@ class AuthUseCase {
       throw new InvalidParamError('loadUserByEmailRepository')
     }
 
-    await this.loadUserByEmailRepository.load(email)
+    const user = await this.loadUserByEmailRepository.load(email)
+
+    if (!user) {
+      return null
+    }
   }
 }
 
@@ -80,5 +84,18 @@ describe('Auth UseCase', () => {
     expect(promise).rejects.toThrow(
       new InvalidParamError('loadUserByEmailRepository')
     )
+  })
+
+  test('should return null if LoadUserByEmailRepository returns null', async () => {
+    const { sut, loadUserByEmailRepositorySpy } = makeSut()
+
+    loadUserByEmailRepositorySpy.load = async () => null
+
+    const accessToken = await sut.auth(
+      'invalid_email@email.com',
+      'any_password'
+    )
+
+    expect(accessToken).toBe(null)
   })
 })

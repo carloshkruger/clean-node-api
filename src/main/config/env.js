56dbb7a81f9925815285070d3675cad6a62fa59b
Carlos H Kruger <carloshkruger@gmail.com>
2021-02-28 11:06:03 -0300

    fix mongodb url and app port

--- src/main/config/env.js
+++ src/main/config/env.js
@@ -1,4 +1,7 @@
 module.exports = {
-  mongoUrl: process.env.MONGO_URL || 'mongodb://localhost:/clean-node-api',
-  tokenSecret: process.env.TOKEN_SECRET || 'secret'
+  mongoUrl:
+    process.env.MONGO_URL ||
+    'mongodb+srv://carlos:11235813@cluster0.fxyhw.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+  tokenSecret: process.env.TOKEN_SECRET || 'secret',
+  port: process.env.PORT || 5858
 }

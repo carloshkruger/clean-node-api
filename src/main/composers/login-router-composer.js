bb549ca89269b6cf43bac40c76b02479f81e8c48
Carlos H Kruger <carloshkruger@gmail.com>
2021-02-28 11:10:02 -0300

    improve LoginRouterComposer

--- src/main/composers/login-router-composer.js
+++ src/main/composers/login-router-composer.js
@@ -7,17 +7,21 @@ const Encrypter = require('../../utils/helpers/encrypter')
 const TokenGenerator = require('../../utils/helpers/token-generator')
 const { tokenSecret } = require('../config/env')
 
-const encrypter = new Encrypter()
-const tokenGenerator = new TokenGenerator(tokenSecret)
-const loadUserByEmailRepository = new LoadUserByEmailRepository()
-const updateAccessTokenRepository = new UpdateAccessTokenRepository()
-const authUseCase = new AuthUseCase({
-  loadUserByEmailRepository,
-  updateAccessTokenRepository,
-  encrypter,
-  tokenGenerator
-})
-const emailValidator = new EmailValidator()
-const loginRouter = new LoginRouter({ authUseCase, emailValidator })
+module.exports = class LoginRouterComposer {
+  static compose() {
+    const encrypter = new Encrypter()
+    const tokenGenerator = new TokenGenerator(tokenSecret)
+    const loadUserByEmailRepository = new LoadUserByEmailRepository()
+    const updateAccessTokenRepository = new UpdateAccessTokenRepository()
+    const authUseCase = new AuthUseCase({
+      loadUserByEmailRepository,
+      updateAccessTokenRepository,
+      encrypter,
+      tokenGenerator
+    })
+    const emailValidator = new EmailValidator()
+    const loginRouter = new LoginRouter({ authUseCase, emailValidator })
 
-module.exports = loginRouter
+    return loginRouter
+  }
+}

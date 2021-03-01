c2051331868d04067618fc4b15f380b9ade3072a
Carlos H Kruger <carloshkruger@gmail.com>
2021-03-01 14:33:13 -0300

    move mongodb uri to .env file

--- src/main/config/env.js
+++ src/main/config/env.js
@@ -1,7 +1,5 @@
 module.exports = {
-  mongoUrl:
-    process.env.MONGO_URL ||
-    'mongodb+srv://carlos:11235813@cluster0.fxyhw.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+  mongoUrl: process.env.MONGO_URL || '',
   tokenSecret: process.env.TOKEN_SECRET || 'secret',
   port: process.env.PORT || 5858
 }
